16f6de6d63626bfc593acc38350b5b75baf79e72
ArjunMurlidhar <murlidhar.4@buckeyemail.osu.edu>
2026-05-26 06:08:53 -0400

    Merge upstream/general

--- src/classes/random.cpp
+++ src/classes/random.cpp
@@ -1,4 +1,3 @@
-
 #include "random.h"
 #include "random_backend.h"
 #include <gsl/gsl_rng.h>
@@ -8,99 +7,31 @@
 #include <iostream>
 #include <cmath>
 
-#define IA 16807
-#define IM 2147483647
-#define AM (1.0/IM)
-#define IQ 127773
-#define IR 2836
-#define NTAB 32
-#define NDIV (1+(IM-1)/NTAB)
-#define EPS 1.2e-7
-#define RNMX (1.0 - EPS)
+// Fallback implementations for numerical recipes random functions
+// Uses GSL when Numerical Recipes are not available
+// The actual implementations should be provided by the user
 
-#define IM1a 2147483563
-#define IM2a 2147473399
-#define AMa (1.0/IM1a)
-#define IMM1a (IM1a-1)
-#define IA1a 40014
-#define IA2a 40692
-#define IQ1a 53668
-#define IQ2a 52774
-#define IR1a 12211
-#define IR2a 3791
-#define NTABa 32
-#define NDIVa (1+IMM1a/NTABa)
-#define EPSa 1.2e-7
-#define RNMXa (1.0-EPSa)
+// Global GSL random number generator for fallback
+static gsl_rng* gsl_rng_fallback = nullptr;
 
-#define MBIG 1000000000
-#define MSEED 161803398
-#define MZ 0
-#define FAC (1.0/MBIG)
-
-float ran1(long *idum){
-
-  int j;
-  long k;
-  static long iy=0;
-  static long iv[NTAB];
-  float temp;
-
-  if (*idum <= 0 || !iy) {
-    if (-(*idum) < 1) *idum=1;
-    else *idum = -(*idum);
-    for (j=NTAB+7;j>=0;j--){
-      k = (*idum)/IQ;
-      *idum = IA*(*idum - k*IQ)-IR*k;
-      if (*idum<0) *idum += IM;
-      if (j<NTAB) iv[j] = *idum;
+// Initialize fallback RNG if not already done
+static void init_fallback_rng() {
+    if (gsl_rng_fallback == nullptr) {
+        gsl_rng_fallback = gsl_rng_alloc(gsl_rng_mt19937);
+        gsl_rng_set(gsl_rng_fallback, 12345); // Default seed
     }
-    iy = iv[0];
-  }
-
-  k= (*idum)/IQ;
-  *idum = IA*(*idum-k*IQ)-IR*k;
-  if (*idum < 0 ) *idum += IM;
-  j = iy/NDIV;
-  iy = iv[j];
-  iv[j] = *idum;
-  if ((temp=AM*iy) > RNMX) return RNMX;
-  else return temp;
 }
 
-float gasdev(long *idum){
-
-  float ran2(long *idum);
-  static int iset = 0;
-  static float gset;
-  float fac,rsq,v1,v2;
-  
-  if (iset==0){
-    do { 
-      v1=2.0*ran2(idum)-1.0;
-      v2=2.0*ran2(idum)-1.0;
-      rsq = v1*v1+v2*v2;
+// Cleanup function (called at program exit)
+static void cleanup_fallback_rng() {
+    if (gsl_rng_fallback != nullptr) {
+        gsl_rng_free(gsl_rng_fallback);
+        gsl_rng_fallback = nullptr;
     }
-    while (rsq >= 1.0 || rsq ==0.0);
-    fac = sqrt(-2.0*log(rsq)/rsq);
-    gset = v1*fac;
-    iset = 1;
-    return v2*fac;
-  }
-  
-  else {
-    iset = 0;
-    return gset;
-  }
 }
 
-float ran2(long *idum){
-  int j;
-  long k;
-  static long idum2=123456789;
-  static long iy=0;
-  static long iv[NTABa];
-  float temp;
+// Register cleanup function
+static int dummy = (atexit(cleanup_fallback_rng), 0);
 
 namespace {
 float bounded_uniform_float() {
